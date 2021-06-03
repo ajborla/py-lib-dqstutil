0f06486dc88c2e4b4fb52219144fc546cba77618
ajborla <ajborla@bigpond.com>
2021-06-04 09:40:05 +1000

    feat: create dqstutil module
    Add auxialiary function skeletons:
    - is_numeric
    - is_possible_date
    - is_possible_numeric
    - determine_column_type
    
    Add inspect_dataset function skeleton.

---
+++ dqtools.py
@@ -0,0 +1,15 @@
+def is_numeric(s):
+    False
+
+def is_possible_date(s):
+    False
+
+def is_possible_numeric(s):
+    False
+
+def determine_column_type(s):
+    pass
+
+def inspect_dataset(dataset, header, generate_report=True):
+    pass
+
