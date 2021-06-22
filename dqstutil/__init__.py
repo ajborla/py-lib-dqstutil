3faccdad315b43a23dced07b9400588a747820d9
ajborla <ajborla@bigpond.com>
2021-06-23 09:02:08 +1000

    refactor(dqstutil): convert module to package
    Add '__init__.py' to 'dqstutil' directory so converting it to a
     package. Also, export all functions via '__all__' allowing users to
     utilise unardorned function names via 'import dqstutil *'.

---
+++ dqstutil/__init__.py
@@ -0,0 +1,26 @@
+from .dqstutil import \
+    _is_valid_colnames, is_numeric, is_possible_date, \
+    is_possible_numeric, determine_column_type, \
+    inspect_dataset, extract_unique_values, \
+    gen_freq_table, load_csv_dataset, add_column, \
+    remove_column, modify_column, transform_column, \
+    remove_columns, extract_row_range, extract_rows
+
+__all__ = [
+    '_is_valid_colnames',
+    'is_numeric',
+    'is_possible_date',
+    'is_possible_numeric',
+    'determine_column_type',
+    'inspect_dataset',
+    'extract_unique_values',
+    'gen_freq_table',
+    'load_csv_dataset',
+    'add_column',
+    'remove_column',
+    'modify_column',
+    'transform_column',
+    'remove_columns',
+    'extract_row_range',
+    'extract_rows'
+]
