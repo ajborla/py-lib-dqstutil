e944e61cb892f34b8e946f8cc97442e6f2c13fe8
ajborla <ajborla@bigpond.com>
2021-06-23 10:42:32 +1000

    chore(build): add dynamic setup script, 'setup.py'
    Add minimal setup script with package-specific metadata for building a
     test distribution.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dqstutil-ajborla",
+    version="1.0.0",
+    author="Anthony J. Borla",
+    author_email="ajborla@bigpond.com",
+    description="A small suite of data manipulation routines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+    url="https://github.com/ajborla/dqstutil-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/ajborla/dqstutil-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=("test", "docs")),
+    python_requires=">=3.8",
+)
