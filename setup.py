f756aa524849bf882dbdf638cadcb4847c625221
ajborla <ajborla@bigpond.com>
2021-07-02 09:25:18 +1000

    chore(build): cleanup dynamic setup script

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ import os
 import sys
 
 
+# Anchor reference
 _here = os.path.abspath(os.path.dirname(__file__))
 
+# Entire package README forms descrioption string
 with open(os.path.join(_here, "README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Package version will be read from a custom `version` file
 version = {}
 with open(os.path.join(_here, "dqstutil", "version.py"), "r", encoding="utf-8") as fh:
     exec(fh.read(), version)
 
+# Declare any multi-use data here
 author = "Anthony J. Borla"
 author_email = "ajborla@bigpond.com"
 
+# Create setup options
 setup(
     name="dqstutil-ajborla",
     version=version["__version__"],
@@ -31,11 +36,11 @@ setup(
         "Bug Tracker": "https://github.com/ajborla/py-lib-dqstutil/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
     ],
-    use_scm_version=False,
     packages=find_packages(exclude=("test", "docs", "examples")),
     python_requires=">=3.8",
     test_suite = "test.test_dqstutil_suite",
