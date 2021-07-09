cf225f3434029dc6d5c046e31f82bd71287d383b
ajborla <ajborla@bigpond.com>
2021-07-10 08:26:36 +1000

    chore(docgen.sh): add parameter checking and globals

--- docgen.sh
+++ docgen.sh
@@ -4,3 +4,11 @@
 which python3 >/dev/null || { echo python3 not installed; exit 1; }
 which node >/dev/null || { echo node not installed; exit 1; }
 which git >/dev/null || { echo git not installed; exit 1; }
+
+# Process shell parameters
+[ -n "$1" ] \
+    && DOCDIR=$(readlink -f "$1") \
+    || { echo Missing documents directory; exit 1; }
+
+# Set globals
+TITLE='dqstutil - (d)ata (q)uery and (st)atistical (util)ities'
