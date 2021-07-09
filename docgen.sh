ae1524e5957181a3bea638606f4dbabcd7215649
ajborla <ajborla@bigpond.com>
2021-07-10 09:39:19 +1000

    chore(docgen.sh): add first processing step

--- docgen.sh
+++ docgen.sh
@@ -12,3 +12,14 @@ which git >/dev/null || { echo git not installed; exit 1; }
 
 # Set globals
 TITLE='dqstutil - (d)ata (q)uery and (st)atistical (util)ities'
+
+# `doc2md` checks and tasks
+python3 -m pip list | grep doc2md >/dev/null \
+    && REMOVE_DOC2MD=1 \
+    || { python3 -m pip install doc2md; REMOVE_DOC2MD=0; }
+python3 -m doc2md -a -t "${TITLE}" dqstutil > ${DOCDIR}/README.md
+[ ${REMOVE_DOC2MD} -eq 0 ] \
+    && python3 -m pip uninstall doc2md
+
+# Fix `doc2md` conversion anomalies
+sed -i 's/^### /## /' ${DOCDIR}/README.md
